fd4913e82f4532b2e9c415ef2bcc55f9b736599f
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-11 10:06:53 +0000

    Add files via upload

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,17 @@
+import mongoose from 'mongoose'
 
+export async function connectDB() {
+  const uri = process.env.MONGODB_URI
+  if (!uri) {
+    console.error('MONGODB_URI manquant dans les variables d\'environnement.')
+    process.exit(1)
+  }
+
+  try {
+    await mongoose.connect(uri)
+    console.log('MongoDB connecté.')
+  } catch (err) {
+    console.error('Échec de connexion à MongoDB :', err.message)
+    process.exit(1)
+  }
+}
